dadd07c733c7811d793baad4486b62994d412be3
mahade <mahade.adib45@gmail.com>
2024-05-04 17:10:05 +0600

    delete function added

--- index.js
+++ index.js
@@ -66,6 +66,15 @@ async function run() {
         res.send(result)
     });
 
+    app.put('/bookings')
+
+    app.delete('/bookings/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
