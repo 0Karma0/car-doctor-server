ee2c3764f2062874e4231369ae15e1463cdd653c
mahade <mahade.adib45@gmail.com>
2024-05-04 14:13:47 +0600

    added bookings collection on db

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
 
     const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings')
 
     app.get('/services', async(req, res) => {
         const cursor = serviceCollection.find();
@@ -46,6 +47,14 @@ async function run() {
         const result = await serviceCollection.findOne(query, options);
         res.send(result);
     })
+
+    //bookings
+    app.post('/bookings', async (req, res) => {
+        const booking = req.body;
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+    });
+
 
 
     // Send a ping to confirm a successful connection
